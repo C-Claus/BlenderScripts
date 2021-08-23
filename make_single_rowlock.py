cfa40e464cdf4944d9c4e93a9c1ee2bc8a7bcca1
Coen Claus <coenclaus@gmail.com>
2021-08-23 17:33:38 +0200

    rowlock refactoring

--- make_single_rowlock.py
+++ make_single_rowlock.py
@@ -7,9 +7,10 @@ collection_name = bpy.data.collections.new("RowLockCollection")
 def add_curve():
     
     bpy.context.scene.collection.children.link(collection_name)
-
-    #coordinates which describe a b-spine or bezier curve
+    #xyz
     coords_list = [[0,0,0], [10,3,0], [20,3,0],[30,0,0]]
+
+    #coords_list = [[0,0,0], [0,0,0], [0,0,0], [0,0,0]]
 
     # make a new curve
     crv = bpy.data.curves.new('crv', 'CURVE')
@@ -21,6 +22,7 @@ def add_curve():
     # a spline point for each point
     spline.points.add(len(coords_list)-1) # theres already one point by default
 
+
     # assign the point coordinates to the spline points
     for p, new_co in zip(spline.points, coords_list):
         print (p, new_co)
@@ -28,72 +30,38 @@ def add_curve():
 
     # make a new object with the curve
     obj = bpy.data.objects.new('curve_for_rowlock', crv)
-
+    #bpy.context.scene.collection.objects.link(obj)
 
     collection_name.objects.link(obj) 
  
 def add_brick():
     
-   #brick #1   
-   bpy.ops.mesh.primitive_cube_add(location=(0.0, 0.0, 0.0)) 
+   #bpy.context.scene.collection.children.link(collection_name)
+    
+   obj = bpy.ops.mesh.primitive_cube_add(location=(0.0, 0.0, 0.0)) 
+
    bpy.context.object.name = "brick"
+   
+   #bpy.context.object.scale = (0.21, 0.1, 0.5)
+   
    bpy.context.object.scale = (0.21, 0.5, 0.1)
    
-   #brick #2
-   bpy.ops.mesh.primitive_cube_add(location=(0.0,0.8, 0.0)) 
-   bpy.context.object.name = "brick_2"
-   bpy.context.object.scale = (0.21, 0.25, 0.1)
-   
-   #brick 3
-   bpy.ops.mesh.primitive_cube_add(location=(0.45,-0.25, 0.0)) 
-   bpy.context.object.name = "brick_3"
-   bpy.context.object.scale = (0.21, 0.25, 0.1)
-   
-   #brick 4
-   bpy.ops.mesh.primitive_cube_add(location=(0.45,0.55, 0.0)) 
-   bpy.context.object.name = "brick_4"
-   bpy.context.object.scale = (0.21, 0.5, 0.1)
-   
-   obj = bpy.context.active_object
-   #bpy.ops.collection.objects_remove_all()
-   
-   scene = bpy.context.scene
-   
-   obs = []
-   
-   for ob in scene.objects:
-        # whatever objects you want to join...
-        if ob.type == 'MESH':
-            obs.append(ob)
-            
-   ctx = bpy.context.copy()          
-   ctx['active_object'] = obs[0]  
-   ctx['selected_editable_objects'] = obs
-   
-   bpy.ops.object.join(ctx)
-   
-             
-   #print (obs)        
    #collection_name.objects.link(obj) 
-  
    
 
 def add_array():
-    
-  
-    bpy.context.view_layer.objects.active = bpy.data.objects['brick']
-    bpy.ops.object.modifier_add(type='ARRAY')
-
+    bpy.ops.object.modifier_add(type="ARRAY")
     
 
-    bpy.data.objects[0].modifiers["Array"].count = 11
+    bpy.data.objects[0].modifiers["Array"].count = 24
     
     #xyz = [0][1][2]
     bpy.data.objects[0].modifiers["Array"].relative_offset_displace[0] = 1.0
     
     bpy.context.object.modifiers["Array"].use_constant_offset = True
     
-    bpy.data.objects[0].modifiers["Array"].constant_offset_displace[0] = 0.2
+    bpy.data.objects[0].modifiers["Array"].constant_offset_displace[0] = 0.1
+    
     
     
     bpy.ops.object.modifier_add(type="CURVE")
@@ -102,10 +70,10 @@ def add_array():
     bpy.context.object.modifiers["Curve"].object = bpy.data.objects["curve_for_rowlock"]
     
     #apply modifiers
-    #bpy.ops.object.modifier_apply(modifier="Array")
-    #bpy.ops.object.modifier_apply(modifier="Curve")
-
+    bpy.ops.object.modifier_apply(modifier="Array")
+    bpy.ops.object.modifier_apply(modifier="Curve")
     bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+
 
 
 
