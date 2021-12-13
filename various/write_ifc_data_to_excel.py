ba532c48bf77565834460f10c3e17dfa5d12c81c
Coen Claus <cclaus@4ps.nl>
2021-12-13 10:05:38 +0100

    added import ifc into funciton

--- various/write_ifc_data_to_excel.py
+++ various/write_ifc_data_to_excel.py
@@ -1,40 +1,37 @@
 import os
-import pandas
+#import pandas
 import openpyxl
 import ifcopenshell
-import csv
+#import csv
 import xlsxwriter
 
 import bpy
 import logging
 import blenderbim.bim.import_ifc
 
-
 from openpyxl import load_workbook
-
 from blenderbim.bim.ifc import IfcStore
-
-
-
 
 #excel_file = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
 #os.startfile(excel_file)
 
-ifc_file_path = "C:\\Algemeen\\07_prive\\hooiberg.ifc"
+#ifc_file_path = "C:\\Algemeen\\07_prive\\hooiberg.ifc"
+ifc_file_path = "C:\\Algemeen\\07_prive\\00_BlenderBIM_IDM\\00_ifc\\origineel\\IFC Schependomlaan.ifc"
 excel_file_path = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
 
-
-#ifc_import_settings = blenderbim.bim.import_ifc.IfcImportSettings.factory(bpy.context, ifc_file_path, logging.getLogger('ImportIFC'))
-#ifc_importer = blenderbim.bim.import_ifc.IfcImporter(ifc_import_settings)
-#ifc_importer.execute()
-
-ifc_file = ifcopenshell.open(ifc_file_path)
-
-products = ifc_file.by_type('IfcProduct')
+def import_ifc(ifc_file):
+    ifc_import_settings = blenderbim.bim.import_ifc.IfcImportSettings.factory(bpy.context, ifc_file, logging.getLogger('ImportIFC'))
+    ifc_importer = blenderbim.bim.import_ifc.IfcImporter(ifc_import_settings)
+    ifc_importer.execute()
 
 
-def write_to_excel():
-    workbook = xlsxwriter.Workbook("C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx")
+def write_to_excel(ifc_file, excel_file):
+    
+    ifc_file = ifcopenshell.open(ifc_file_path)
+    products = ifc_file.by_type('IfcProduct')
+    
+    
+    workbook = xlsxwriter.Workbook(excel_file)
     worksheet = workbook.add_worksheet('IfcProduct')
 
     #cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
@@ -58,27 +55,17 @@ def write_to_excel():
         
     workbook.close()
 
-    excel_file = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
+    #excel_file = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
     os.startfile(excel_file)
 
 
-
-
-
-
 def get_filtered_data_from_excel(excel_file):
-    wb = load_workbook(excel_file) # use the actual path of your workbook
-    ws = wb['IfcProduct'] # use your sheet name instead of Bar
+    wb = load_workbook(excel_file)
+    ws = wb['IfcProduct'] 
     
     global_id_filtered_list = []
 
-    # iterate over all the rows in the sheet
-    for row in ws: 
-        # use the row only if it has not been filtered out (i.e., it's not hidden)
-        #if ws.row_dimensions[row[0].row].hidden == False:
-        #    print (row) # ...or do what you need
-   
-            
+    for row in ws:     
         if ws.row_dimensions[row[0].row].hidden == False:
             for cell in row:  
                 if cell in ws['A']:  
@@ -89,30 +76,30 @@ def get_filtered_data_from_excel(excel_file):
                 
                 
     
-                
-                
+                               
 def hide_isolate_in_view(guid_list):
-    print ('hide isolate in view')
-    
+   
     #bpy.ops.object.hide_view_clear()
-  
     bpy.ops.object.select_all(action='DESELECT')
 
-    
     for guid in guid_list:
-        
-        #bpy.context.scene.BIMSearchProperties.global_id = guid
         bpy.ops.bim.select_global_id(global_id=guid)
 
 
-    #bpy.ops.object.hide_view_set(unselected=True)
+    bpy.ops.object.hide_view_set(unselected=True)
     #bpy.data.objects.foreach_set("hide_viewport", (True,) * len(bpy.data.objects))
 
     #obj = bpy.context.active_object
     #IfcStore.get_file().by_id(obj.BIMObjectProperties.ifc_definition_id).GlobalId
     
             
-#get_filtered_data_from_excel(excel_file=excel_file_path)        
+       
 
-#write_to_excel()
+#1 needs to happen first time running the script
+#import_ifc(ifc_file=ifc_file_path)
+
+#2 needs to happen furst time running the script
+#write_to_excel(ifc_file=ifc_file_path, excel_file=excel_file_path)
+
+#3 function is used everytime when filtering, don't need to be used first time
 hide_isolate_in_view(guid_list=get_filtered_data_from_excel(excel_file=excel_file_path) )    
