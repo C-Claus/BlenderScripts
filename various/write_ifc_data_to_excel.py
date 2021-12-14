9e222df5eb8f74da47021fa81454efc6699cc406
Coen Claus <cclaus@4ps.nl>
2021-12-14 13:58:51 +0100

    some refactoring

--- various/write_ifc_data_to_excel.py
+++ various/write_ifc_data_to_excel.py
@@ -1,33 +1,19 @@
 import os
-#import pandas
+import bpy
 import openpyxl
 import ifcopenshell
-#import csv
 import xlsxwriter
 
-import bpy
 import logging
 import blenderbim.bim.import_ifc
 
 from openpyxl import load_workbook
 from blenderbim.bim.ifc import IfcStore
 
-#excel_file = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
-#os.startfile(excel_file)
-
-#ifc_file_path = "C:\\Algemeen\\07_prive\\hooiberg.ifc"
-ifc_file_path = "C:\\Algemeen\\07_prive\\00_BlenderBIM_IDM\\00_ifc\\origineel\\IFC Schependomlaan.ifc"
-excel_file_path = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
-
-def import_ifc(ifc_file):
-    ifc_import_settings = blenderbim.bim.import_ifc.IfcImportSettings.factory(bpy.context, ifc_file, logging.getLogger('ImportIFC'))
-    ifc_importer = blenderbim.bim.import_ifc.IfcImporter(ifc_import_settings)
-    ifc_importer.execute()
-
 
 def write_to_excel(ifc_file, excel_file):
     
-    ifc_file = ifcopenshell.open(ifc_file_path)
+    ifc_file = ifcopenshell.open(ifc_file)
     products = ifc_file.by_type('IfcProduct')
     
     
@@ -54,8 +40,6 @@ def write_to_excel(ifc_file, excel_file):
         worksheet.write('C' + str(i+2), str(product.Name))
         
     workbook.close()
-
-    #excel_file = "C:\\Algemeen\\07_prive\\06_excel_vba\\BlenderBIM.xlsx"
     os.startfile(excel_file)
 
 
@@ -64,42 +48,32 @@ def get_filtered_data_from_excel(excel_file):
     ws = wb['IfcProduct'] 
     
     global_id_filtered_list = []
+    
+    #wb.save(filename = excel_file)
 
     for row in ws:     
         if ws.row_dimensions[row[0].row].hidden == False:
             for cell in row:  
                 if cell in ws['A']:  
-                    print (cell.value) 
                     global_id_filtered_list.append(cell.value)
                     
     return global_id_filtered_list[1:]
                 
                 
-    
-                               
-def hide_isolate_in_view(guid_list):
+                             
+def select_IFC_elements_in_blender(guid_list):
    
-    #bpy.ops.object.hide_view_clear()
-    bpy.ops.object.select_all(action='DESELECT')
-
     for guid in guid_list:
         bpy.ops.bim.select_global_id(global_id=guid)
-
-
-    bpy.ops.object.hide_view_set(unselected=True)
-    #bpy.data.objects.foreach_set("hide_viewport", (True,) * len(bpy.data.objects))
-
-    #obj = bpy.context.active_object
-    #IfcStore.get_file().by_id(obj.BIMObjectProperties.ifc_definition_id).GlobalId
-    
-            
+        
+         
        
+excel_file_path = (os.path.dirname(IfcStore.path) + '\\' + (os.path.basename(IfcStore.path).replace('.ifc','.xlsx')) )
 
-#1 needs to happen first time running the script
-#import_ifc(ifc_file=ifc_file_path)
+#1 needs to  happen on user input
+write_to_excel(ifc_file=IfcStore.path, excel_file=excel_file_path)
 
-#2 needs to happen furst time running the script
-#write_to_excel(ifc_file=ifc_file_path, excel_file=excel_file_path)
+#2 check if excel is running and saved before using this function
+#select_IFC_elements_in_blender(guid_list=get_filtered_data_from_excel(excel_file=excel_file_path) )   
 
-#3 function is used everytime when filtering, don't need to be used first time
-hide_isolate_in_view(guid_list=get_filtered_data_from_excel(excel_file=excel_file_path) )    
+
