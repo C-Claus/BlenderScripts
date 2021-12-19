6fe413f627af6af2543a85ab396864963d005a18
Coen Claus <cclaus@4ps.nl>
2021-12-19 18:42:02 +0100

    added gui

--- BlenderBIMExcel/BlenderBIM_to_Excel_GUI.py
+++ BlenderBIMExcel/BlenderBIM_to_Excel_GUI.py
@@ -1,23 +1,60 @@
+import sys 
+sys.path.append('C:\Python 39\Lib\site-packages')
+import bpy
+
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
+                             QAction, QFileDialog,QMenu, QApplication,
+                             QGridLayout, QLabel, QLineEdit, QWidget, QPushButton)
+                             
+from PyQt5.QtGui import QIcon
 import sys
-from PyQt5 import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from pathlib import Path
 
-app = QApplication(sys.argv)
-def Start():
-    m = myWindow()
-    m.showWid()
-    sys.exit(app.exec())
 
-class myWindow:
-  def __init__(self):
-    self.window = QWidget()
-    self.window.setWindowTitle("BlenderBIM <-> Excel")
-    self.window.setFixedWidth(600)
-    self.window.setStyleSheet("background: #18BEBE;")
+from blenderbim.bim.ifc import IfcStore
 
-  def showWid(self):
-    self.window.show()
 
-if __name__ == "__main__":
-    Start()
+class ExportWindow(QMainWindow, QWidget):
+    def __init__(self, parent=None):
+        super(ExportWindow, self).__init__(parent)
+        
+        self.initUI()
+        
+     
+    def initUI(self):
+  
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        open_file = QAction('Open Excel', self)
+        open_file.triggered.connect(self.open_excel)
+        fileMenu.addAction(open_file)
+ 
+        self.setGeometry(1200, 100, 350, 600)
+        self.setWindowTitle('BlenderBIM Excel')
+        self.show()
+
+
+    def open_excel(self):
+        print("Open Excel")
+        
+        home_dir = str(IfcStore.path)
+        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+        
+                
+
+try:
+    app = QtWidgets.QApplication(sys.argv) 
+    window = ExportWindow()
+    window.show()
+
+    # https://stackoverflow.com/questions/28060218/where-is-pyqt-event-loop-running
+
+except Exception as e:
+    print ('error')
+    print (e)
