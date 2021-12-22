3a343f563342b288d119c5b714d1175b6b04d702
Coen Claus <cclaus@4ps.nl>
2021-12-22 13:43:33 +0100

    some refactoring

--- BlenderBIMExcel/BlenderBIM_to_Excel_ui.py
+++ BlenderBIMExcel/BlenderBIM_to_Excel_ui.py
@@ -1,14 +1,32 @@
 import os
 import bpy
-
 from bpy.props import StringProperty, BoolProperty
 from bpy_extras.io_utils import ImportHelper 
 from bpy.types import Operator
 
+import openpyxl
+from openpyxl import load_workbook
+
+import blenderbim.bim.import_ifc
+from blenderbim.bim.ifc import IfcStore
+import blenderbim.tool as tool
+
+class WriteToExcel(bpy.types.Operator):
+    """Write IFC data to Excel"""
+    bl_idname = "object.write_to_excel"
+    bl_label = "Simple Object Operator"
 
 
-class SimpleOperator(bpy.types.Operator, ImportHelper):
-    """Tooltip"""
+    def execute(self, context):
+        print("Write to Excel")
+        
+    
+        return {'FINISHED'}
+
+
+
+class OpenExcelFile(bpy.types.Operator, ImportHelper):
+    """Open an existing Excel file"""
     bl_idname = "object.simple_operator"
     bl_label = "Open Excel"
     
@@ -40,16 +58,64 @@ class SimpleOperator(bpy.types.Operator, ImportHelper):
         return {'FINISHED'}
     
 class FilterIFCElements(bpy.types.Operator):
-    """Tooltip"""
+    """Show the IFC elements you filtered in Excel"""
     bl_idname = "object.filter_ifc_elements"
-    bl_label = "Simple Object Operator"
+    bl_label = "select the IFC elements"
 
 
     def execute(self, context):
         print("filter IFC elements")
         
         print (excel_file)
+        
+        workbook_openpyxl = load_workbook(excel_file)
+        worksheet_openpyxl = workbook_openpyxl['IfcProduct'] 
+        
+        global_id_filtered_list = []
+
+        for row in worksheet_openpyxl:     
+            if worksheet_openpyxl.row_dimensions[row[0].row].hidden == False:
+                for cell in row:  
+                    if cell in worksheet_openpyxl['A']:  
+                        global_id_filtered_list.append(cell.value)
+                        
+         
+        
+        outliner = next(a for a in bpy.context.screen.areas if a.type == "OUTLINER") 
+        outliner.spaces[0].show_restrict_column_viewport = not outliner.spaces[0].show_restrict_column_viewport
+        
+        bpy.ops.object.select_all(action='DESELECT')
+      
+        for obj in bpy.context.view_layer.objects:
+            element = tool.Ifc.get_entity(obj)
+            if element is None:        
+                obj.hide_viewport = True
+                continue
+            data = element.get_info()
+          
+            obj.hide_viewport = data.get("GlobalId", False) not in global_id_filtered_list[1:]
+
+        bpy.ops.object.select_all(action='SELECT') 
+        
         return {'FINISHED'}
+    
+ 
+class UnhideIFCElements(bpy.types.Operator):
+    """Unhide all IFC elements"""
+    bl_idname = "object.unhide_all"
+    bl_label = "Unhide All"
+
+
+    def execute(self, context):
+        print("Unhide all")
+        
+        for obj in bpy.data.objects:
+            obj.hide_viewport = False 
+        
+    
+        return {'FINISHED'}      
+               
+     
     
 
 
@@ -64,25 +130,32 @@ class BlenderBIMExcelPanel(bpy.types.Panel):
     def draw(self, context):
         
         
-
-        self.layout.operator(SimpleOperator.bl_idname, text="Open Excel File", icon="FILE_FOLDER")
+        self.layout.operator(WriteToExcel.bl_idname, text="Write to Excel", icon="FILE")
+        self.layout.operator(OpenExcelFile.bl_idname, text="Open Excel File", icon="FILE_FOLDER")
         
         self.layout.operator(FilterIFCElements.bl_idname, text="Filter IFC elements", icon="FILTER")
+        #self.layout.operator(UnhideIFCElements.bl_idname, text="Unhide IFC elements", icon="LAMP")
+        
+        
         # Tip : enable Icon Viewer addon to have a list of available icons
         # https://docs.blender.org/manual/en/latest/addons/development/icon_viewer.html
 
 
 
 def register():
-    bpy.utils.register_class(SimpleOperator)
+    bpy.utils.register_class(WriteToExcel)
+    bpy.utils.register_class(OpenExcelFile)
     bpy.utils.register_class(FilterIFCElements)
+    bpy.utils.register_class(UnhideIFCElements)
     bpy.utils.register_class(BlenderBIMExcelPanel)
 
 
-def unregister():
+def unregister(): 
+    bpy.utils.unregister_class(WriteToExcel)
+    bpy.utils.unregister_class(OpenExcelFile)
+    bpy.utils.unregister_class(FilterIFCElements)
+    bpy.utils.unregister_class(UnhideIFCElements)
     bpy.utils.unregister_class(BlenderBIMExcelPanel)
-    bpy.utils.unregister_class(SimpleOperator)
-    bpy.utils.register_class(FilterIFCElements)
 
 
 if __name__ == "__main__":
