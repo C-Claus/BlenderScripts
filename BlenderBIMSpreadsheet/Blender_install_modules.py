91a8e266e7947eb946265ca993739bd9960d6a3a
Coen Claus <cclaus@4ps.nl>
2022-10-01 11:49:17 +0200

    added types

--- BlenderBIMSpreadsheet/Blender_install_modules.py
+++ BlenderBIMSpreadsheet/Blender_install_modules.py
@@ -15,8 +15,8 @@ py_exec = str(sys.executable)
 #subprocess.call([py_exec, "-m", "pip", "install", "--upgrade", "pip" ])
 
 # install packages
-subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "openpyxl"])
-#subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "pandas"])
+#subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "openpyxl"])
+subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "pandas"])
 #subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "xlsxwriter"])
 #subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "pyexcel-ods3"])
 #subprocess.call([py_exec,"-m", "pip", "install", f"--target={py_exec[:-14]}" + "lib", "uno"])
