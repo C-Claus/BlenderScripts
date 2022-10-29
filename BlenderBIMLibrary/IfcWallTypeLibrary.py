500ed0a6a57f447745e9fac3ac3b82d5d8d7945a
Coen Claus <cclaus@4ps.nl>
2022-10-29 11:47:43 +0200

    changed to meters

--- BlenderBIMLibrary/IfcWallTypeLibrary.py
+++ BlenderBIMLibrary/IfcWallTypeLibrary.py
@@ -19,7 +19,7 @@ def create_ifcwalltype_library(ifc_element, ifc_element_type, element_name, ifc_
     library = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProjectLibrary", name="BlenderBIM Demo Library")
 
     ifcopenshell.api.run( "project.assign_declaration", ifc_file, definition=library, relating_context=project)
-    ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "MILIMETERS"})
+    ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "METERS"})
 
     material = ifcopenshell.api.run("material.add_material", ifc_file, name=material_name)
 
