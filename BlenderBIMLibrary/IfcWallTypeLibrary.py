139a3114527d521e365651dccb7af159b9cc0e83
Coen Claus <cclaus@4ps.nl>
2022-11-02 21:43:31 +0100

    added csv and added classification

--- BlenderBIMLibrary/IfcWallTypeLibrary.py
+++ BlenderBIMLibrary/IfcWallTypeLibrary.py
@@ -2,32 +2,23 @@ import ifcopenshell.api
 import pandas as pd
 import bpy
 
-ifc_file = ifcopenshell.api.run("project.create_file")
-project = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProject", name="BlenderBIM Demo")
-library = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProjectLibrary", name="BlenderBIM Demo Library")
-
-ifcopenshell.api.run( "project.assign_declaration", ifc_file, definition=library, relating_context=project)
-ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "METERS"})
 
 def get_wall_library(csv_library):
        
     library_data_frame = pd.read_csv(csv_library, ";")
     
-    #for index, row in library_data_frame.iterrows():
-        #print(row['IfcElement'])
-    
     return library_data_frame
     
 
-def create_ifcwalltype_library(ifc_element, ifc_element_type, element_name, ifc_material_layer_set, material_name,pset_common, is_external, load_bearing, fire_rating, width, rgb):
+def create_ifcwalltype_library(ifc_element, ifc_element_type, element_name, ifc_material_layer_set, material_name,classification, item_reference,pset_common, is_external, load_bearing, fire_rating, width, rgb):
     ifcopenshell.api.post_listeners = {}
 
-    #ifc_file = ifcopenshell.api.run("project.create_file")
-    #project = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProject", name="BlenderBIM Demo")
-    #library = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProjectLibrary", name="BlenderBIM Demo Library")
+    ifc_file = ifcopenshell.api.run("project.create_file")
+    project = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProject", name="BlenderBIM Demo")
+    library = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProjectLibrary", name="BlenderBIM Demo Library")
 
-    #ifcopenshell.api.run( "project.assign_declaration", ifc_file, definition=library, relating_context=project)
-    #ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "METERS"})
+    ifcopenshell.api.run( "project.assign_declaration", ifc_file, definition=library, relating_context=project)
+    ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "METERS"})
 
     material = ifcopenshell.api.run("material.add_material", ifc_file, name=material_name)
 
@@ -81,37 +72,44 @@ def create_ifcwalltype_library(ifc_element, ifc_element_type, element_name, ifc_
     )
         
         
-        
+    result = ifcopenshell.api.run("classification.add_classification", ifc_file, classification='NLSfb')   
+    
+    ifcopenshell.api.run(
+            "classification.add_reference",
+            ifc_file,
+            product=element,
+            identification=str(item_reference),
+            name=str(classification),
+            classification=result,
+        )
+
 
     ifcopenshell.api.run("project.assign_declaration", ifc_file, definition=element, relating_context=library)
 
-
-    #ifc_file.write("C:\\Algemeen\\00_prive\\08_ifc_bestanden\\ifc_library\\" + str(element_name) + ".ifc")
+    print (element_name)
+    ifc_file.write("C:\\Algemeen\\00_prive\\08_ifc_bestanden\\ifc_library\\" + str(element_name) + ".ifc")
 
 
 ########################################################
 ### Call the methods which loop through the CSV file ###
 ########################################################
-
-
-#csv_wall_library = 'https://raw.githubusercontent.com/C-Claus/BlenderScripts/master/BlenderBIMLibrary/IfcWallLibrary.csv?raw=true'
-
-csv_wall_library = "C:\\Algemeen\\00_prive\\BlenderScripts\\BlenderBIMLibrary\\IfcWallLibrary.csv"
+csv_wall_library = "C:\\Algemeen\\00_prive\\BlenderScripts\\BlenderBIMLibrary\\binnenwanden_bibliotheek.csv"
 get_wall_library(csv_library=csv_wall_library)
 
 for index, row in get_wall_library(csv_library=csv_wall_library).iterrows():
-
     
+
     create_ifcwalltype_library( ifc_element=row['IfcElement'],
                                 ifc_element_type=row['IfcElementType'],
                                 element_name=row['Name'],
                                 ifc_material_layer_set=row['IfcMaterialLayerSet'],
                                 material_name=row['Material'],
+                                classification=row['Classification'],
+                                item_reference=row['ItemReference'],
                                 pset_common=row['Pset_Common'],
                                 is_external=row['IsExternal'],
                                 load_bearing=row['LoadBearing'],
                                 fire_rating=row['FireRating'],
                                 width=row['Width'],
                                 rgb=row['RGB'])
-                             
-ifc_file.write("C:\\Algemeen\\00_prive\\08_ifc_bestanden\\ifc_library\\library.ifc")
+                        
