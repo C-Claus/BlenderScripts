cd98e7cc485561bd6f6d42738f7010d3d4996956
Coen Claus <cclaus@4ps.nl>
2022-11-07 17:21:14 +0100

    added site, building storey and building

---
+++ BlenderBIM_create_beam/create_beam.py
@@ -0,0 +1,102 @@
+import bpy
+import ifcopenshell.api
+
+
+ifc_file = ifcopenshell.api.run("project.create_file")
+project = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcProject", name="BlenderBIM Demo")
+
+#ifcopenshell.api.run( "project.assign_declaration", ifc_file, definition=project, relating_context=project)
+ifcopenshell.api.run("unit.assign_unit", ifc_file, length={"is_metric": True, "raw": "METERS"})
+
+model = ifcopenshell.api.run("context.add_context", ifc_file, context_type="Model")
+#model = ifcopenshell.file()
+plan = ifcopenshell.api.run("context.add_context", ifc_file, context_type="Plan")
+
+
+
+
+    
+representations = {
+    "body": ifcopenshell.api.run(
+        "context.add_context",
+        ifc_file,
+        context_type="Model",
+        context_identifier="Body",
+        target_view="MODEL_VIEW",
+        parent=model,
+    ),
+    "annotation": ifcopenshell.api.run(
+        "context.add_context",
+        ifc_file,
+        context_type="Plan",
+        context_identifier="Annotation",
+        target_view="PLAN_VIEW",
+        parent=plan,
+    ),
+}
+
+site = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcSite", name="My Site")
+building = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcBuilding", name="Building A")
+storey = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcBuildingStorey", name="Ground Floor")
+
+ifcopenshell.api.run("aggregate.assign_object", ifc_file, relating_object=project, product=site)
+ifcopenshell.api.run("aggregate.assign_object", ifc_file, relating_object=site, product=building)
+ifcopenshell.api.run("aggregate.assign_object", ifc_file, relating_object=building, product=storey)
+
+element_name='my_beam'
+material = ifcopenshell.api.run("material.add_material", ifc_file, name='beam_material')
+profile = ifc_file.create_entity("IfcRectangleProfileDef", ProfileType="AREA", XDim=0.5, YDim=0.6)
+
+element = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class='IfcBeamType', name=element_name)
+rel = ifcopenshell.api.run("material.assign_material", ifc_file, product=element, type="IfcMaterialProfileSet")
+profile_set = rel.RelatingMaterial
+material_profile = ifcopenshell.api.run( "material.add_profile", ifc_file, profile_set=profile_set, material=material)
+ifcopenshell.api.run("material.assign_profile", ifc_file, material_profile=material_profile, profile=profile)
+
+#or use:
+
+#material_profile = ifcopenshell.api.run("material.add_profile", ifc_file, profile_set=profile_set, material=material, profile=profile)
+
+
+
+occurrence = ifcopenshell.api.run("root.create_entity", ifc_file, ifc_class="IfcBeam", name=element_name)
+ifcopenshell.api.run("type.assign_type", ifc_file, related_object=occurrence, relating_type=element)
+
+representation = ifcopenshell.api.run("geometry.add_profile_representation", ifc_file, context=representations["body"], profile=profile, depth=3)
+
+
+ifcopenshell.api.run("spatial.assign_container", ifc_file, relating_structure=storey, product=occurrence)
+ifcopenshell.api.run("geometry.assign_representation", ifc_file, product=occurrence, representation=representation)
+
+
+
+
+######################################################################################
+#########################  Write file and load into BlenderBIM #######################
+######################################################################################
+folder_path = "C:\\Algemeen\\00_prive\\BlenderScripts\\BlenderBIM_create_objects\\ifc_library" 
+filename = str(element_name) + ".ifc"
+file_path = (folder_path + filename)
+ifc_file.write(file_path)
+
+def load_ifc_automatically():
+
+    if (bool(ifc_file)) == True:
+        project = ifc_file.by_type('IfcProject')
+        
+        if project is not None:
+            for i in project:
+                collection_name = 'IfcProject/' + i.Name
+                
+            collection = bpy.data.collections.get(str(collection_name))
+             
+            if collection is not None:
+                for obj in collection.objects:
+                    bpy.data.objects.remove(obj, do_unlink=True)
+                    
+                bpy.data.collections.remove(collection)
+                
+        bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)         
+        bpy.ops.bim.load_project(filepath=file_path)
+               
+load_ifc_automatically()
