9fdedad2a4e7671c496ccc970d9abd0c327d1a4b
Coen Claus <cclaus@4ps.nl>
2023-04-21 13:25:22 +0200

    add method for image path

--- BlenderBIM_add_reference_images/operator.py
+++ BlenderBIM_add_reference_images/operator.py
@@ -23,6 +23,13 @@ class AddReferenceImage(bpy.types.Operator):
 
         #dimension_properties = context.scene.dimension_properties
 
+    def add_image_path_to_ifcproperty(self,context, image_path):
+
+        ifc = ifcopenshell.open(IfcStore.path)
+        element = ifc.by_type("IfcBuilding") # Just as an example, you will need to select the elements you want to add properties to yourself.
+        pset = run("pset.add_pset", ifc, product=element, name="Reference Image")
+        run("pset.edit_pset", ifc, pset=pset, properties={"image A": str(image_path), "foo2": "foobaz"})
+
 def register():
     bpy.utils.register_class(AddReferenceImage)
 
--- BlenderBIM_add_reference_images/ui.py
+++ BlenderBIM_add_reference_images/ui.py
@@ -13,6 +13,17 @@ class PANEL_PT_demo(Panel):
 
         image_properties = context.scene.image_properties
 
+        layout = self.layout
+
+        box = layout.box()
+      
+        row = box.row()
+        row.prop(image_properties, 'my_reference_image_A')
+
+        box = layout.box()
+        row = box.row()
+        row.operator("add.referenceimage")
+
 def register():
     bpy.utils.register_class(PANEL_PT_demo)
    
