ec5a58d3419adc73476a472bf74f4da3e7c5419f
Coen Claus <cclaus@4ps.nl>
2023-04-26 13:12:32 +0200

    added custom properties

--- BlenderBIM_add_reference_images/properties.py
+++ BlenderBIM_add_reference_images/properties.py
@@ -14,11 +14,25 @@ class ImageProperties(bpy.types.PropertyGroup):
                                                                         maxlen=1024,
                                                                         subtype="FILE_PATH")
 
+class CustomItem(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty(name         ="Property",
+                                   description  ="Use the PropertySet name and Property name divided by a .",
+                                   default      ="PropertySet.Property"
+                                   )
+    
+class CustomCollection(bpy.types.PropertyGroup):
+    items: bpy.props.CollectionProperty(type=CustomItem)
+
+
 def register():
+    bpy.utils.register_class(CustomItem)
+    bpy.utils.register_class(CustomCollection)
     bpy.utils.register_class(ImageProperties)
     bpy.types.Scene.image_properties = bpy.props.PointerProperty(type=ImageProperties)
 
 def unregister():
+    bpy.utils.unregister_class(CustomItem)
+    bpy.utils.unregister_class(CustomCollection)
  
     bpy.utils.unregister_class(ImageProperties)
     del bpy.types.Scene.image_properties
